0d1b79c43bed328adeb1c1d27a7f8e678c957360
Guircai <892948690@qq.com>
2022-01-15 17:09:22 +0800

    P89-web源码分析-为啥注释有问号|20220115-17:09

--- springmvc-source-test/src/main/java/com/guircai/web/controller/HelloController.java
+++ springmvc-source-test/src/main/java/com/guircai/web/controller/HelloController.java
@@ -2,15 +2,22 @@ package com.guircai.web.controller;
 
 import com.guircai.web.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @author Guircai
  **/
 //@Controller；如果有父子容器 @Component，默认是在父容器中，还找不到
 //@Component+@RequestMapping
-@RestController
+//@RestController
+@Controller
 public class HelloController {
 
 	public HelloController() {
@@ -24,11 +31,20 @@ public class HelloController {
 	 * RequestMappingHandlerAdapter：怎么反射执行到这个方法。确定每一个参数的值，以及处理返回值等等
 	 *
 	 * @param name
-	 * @return
+	 * @return		请求数据到自定义Object属性的映射需要用到
+	 * 					（WebDataBinder【数据类型转换，绑定错误处理】）Factory
 	 */
 	@GetMapping("/hello") // 所有的xxxMapping都是RequestMapping
-	public String SayHello(String name) {
-		String msg = service.SayHello("MVC");
-		return msg;
+	public String SayHello(String name, // 可以从请求参数中得到
+						   @RequestParam("user")String user, // 可以从请求参数中得到
+						   HttpSession session, HttpServletRequest request,
+						   @RequestHeader("User-Agent")String ua) { // 原生的session对象
+		// @RequestHeader("User-Agent") String ua 获取指定请求头的值
+		String header = request.getHeader("User-Agent");
+		// 方法的签名，到底能写哪些
+		String msg = service.SayHello(user+"MVC"+name);
+		session.setAttribute("msg", "处理成功");
+		// SpringMVC的目标方法能写哪些返回值
+		return "index.jsp";
 	}
 }
